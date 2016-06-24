0436b275c94ddb1cf78511f0d14093adc3c921aa
NEWS1 <1193345101@qq.com>
2016-06-24 09:42:42 +0800

    消息处理机制

--- app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -1,14 +1,58 @@
 package com.example.administrator.myapplication;
 
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView tvTest;
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case 0x0001:
+                    int index = msg.arg1;
+                    tvTest.setText(index+"");
+                    break;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tvTest = (TextView) findViewById(R.id.tvTest);
+    }
+
+    public void test(View view) {
+//        try {
+//            Thread.sleep(20000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+        switch (view.getId()) {
+            case R.id.btnAsync:
+                new Thread() {
+                    public void run() {
+                        for (int i = 0; i < 100; i++) {
+                            Message msg = new Message();
+                            msg.what = 0x0001;
+                            msg.arg1 = i;
+                            handler.sendMessage(msg);
+                            try {
+                                Thread.sleep(1000);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }.start();
+                break;
+        }
     }
 }
